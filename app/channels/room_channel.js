3f009d25dc87dacbc35daed95d7ebcbfdcb04f92
Ryu <goskn88006@yahoo.co.jp>
2022-06-06 17:02:33 +0900

    add some changes on message model and create job

--- app/channels/room_channel.js
+++ app/channels/room_channel.js
@@ -1,19 +1,33 @@
-const chatChannel = consumer.subscriptions.create("RoomChannel", {
-      // ...
-      // これが実行されるとコンシューマになったRoomChannel#speak({ message: message })が呼ばれる
-      speak: function(message) {
-        return this.perform('speak', {
-          message: message
-        });
-      },
-      received: function(data) {
-            return alert(data['message']);
-          }
-    });
-    $(document).on('keypress', '[data-behavior~=room_speaker]', function(event) {
-      if (event.keyCode === 13) {
-        chatChannel.speak(event.target.value);
-        event.target.value = '';
-        return event.preventDefault();
-      }
-    });
+
+
+// $(function() { ... }); で囲むことでレンダリング後に実行される
+// レンダリング前に実行されると $('#messages').data('room_id') が取得できない
+// turbolinks を使っている場合は $(document).on('turbolinks:load', function() { ... }); で囲う
+
+const chatChannel = consumer.subscriptions.create({ channel: 'RoomChannel', room: $('#messages').data('room_id') }, {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
+
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+
+    received: function(data) {
+      return $('#messages').append(data['message']);
+    },
+
+    speak: function(message) {
+      return this.perform('speak', {
+        message: message
+      });
+    }
+  });
+
+  $(document).on('keypress', '[data-behavior~=room_speaker]', function(event) {
+    if (event.keyCode === 13) {
+      chatChannel.speak(event.target.value);
+      event.target.value = '';
+      return event.preventDefault();
+    }
+  });
